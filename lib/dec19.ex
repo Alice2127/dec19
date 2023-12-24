0e952bee705b3524ebcbe6e1ac81f91c62f2b337
Alice2127 <aiokada2128@gmail.com>
2023-12-25 00:37:27 +0900

    TDD-display-1doctestを通すところまで

--- lib/dec19.ex
+++ lib/dec19.ex
@@ -9,5 +9,18 @@ defmodule Dec19 do
       iex> Dec19.sum(2, 3)
       5
   """
-  def sum(a, b), do: a + b
+  def sum(a, b) do
+    a + b
+  end
+
+  @doc """
+  Get the result of adding and display.
+
+  ## Examples
+      iex> Dec19.display(1, 2)
+      "add result: 3"
+  """
+  def display(a, b) do
+    "add result: 3"
+  end
 end
