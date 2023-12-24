f0ce613879f45f1d5c28e6eea2a6f70c727fe3d6
Alice2127 <aiokada2128@gmail.com>
2023-12-25 02:10:36 +0900

    TDD-display-2doctestを通すところまで

--- lib/dec19.ex
+++ lib/dec19.ex
@@ -19,8 +19,17 @@ defmodule Dec19 do
   ## Examples
       iex> Dec19.display(1, 2)
       "add result: 3"
+
+      iex> Dec19.display(2, 3)
+      "add result: 5"
+
   """
   def display(a, b) do
-    "add result: 3"
+    "add result: #{a + b}"
   end
+
+  def display(_, _) do
+    "Invalid input, both arguments must be numbers"
+  end
+
 end
--- lib/dec19_web/controllers/page_controller.ex
+++ lib/dec19_web/controllers/page_controller.ex
@@ -1,9 +1,9 @@
 defmodule Dec19Web.PageController do
   use Dec19Web, :controller
 
-  def home(conn, _params) do
+  def home(conn, params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: false, params: params)
   end
 end
